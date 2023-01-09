28ea47bb514d282bf178c12146154db406850e72
chia7812033 <dan891221@gmail.com>
2023-01-09 10:16:12 +0800

    Display a random meme

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,11 +1,20 @@
 import React from "react"
+import memesData from "../memesData.js"
 
 export default function Meme() {
+  const index = Math.floor(Math.random() * memesData.data.memes.length)
+  const meme = memesData.data.memes[index]
+  console.log(meme)
   return (
-    <form className="meme">
-      <input type="text" value="Shut up" className="meme--input"></input>
-      <input type="text" value="and take my money" className="meme--input"></input>
-      <button className="meme--button">Get a new meme image  🖼</button>
-    </form>
+    <div>
+      <div className="meme">
+        <input type="text" className="meme--input"></input>
+        <input type="text" className="meme--input"></input>
+        <button className="meme--button">Get a new meme image  🖼</button>
+      </div>
+      <div className="meme--box">
+        <img src={meme.url} alt={meme.name} className="meme--img"/>
+      </div>
+    </div>
   )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import React from "react"
 export default function Navbar() {
   return (
     <nav className="nav">
-      <img src="./images/trollface.png" className="nav--img" />
+      <img src="./images/trollface.png" className="nav--img" alt="trollface"/>
       <h3 className="nav--title">Meme Generator</h3>
       <h5 className="nav--subtitle">React Course - Project 3</h5>
     </nav>
